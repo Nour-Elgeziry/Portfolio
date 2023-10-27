30e73dfc4e9a54d76534dc5315f67aa09b8dc434
Nour-Elgeziry <nourahmedelgeziry@gmail.com>
2023-10-27 19:47:21 +0300

    Add initial navbar and home page

---
+++ app/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-teal-900">
+      <a className="btn btn-ghost normal-case text-xl text-white ">Home</a>
+      <a className="btn btn-ghost normal-case text-xl text-white ">About</a>
+      <a className="btn btn-ghost normal-case text-xl text-white ">Projects</a>
+      <a className="btn btn-ghost normal-case text-xl text-white ">Connect</a>
+    </div>
+  );
+};
+
+export default Navbar;
